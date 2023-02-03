c3f7fe6fc4bc5ebe8908e8ab4a39cb0deb8ccd92
fritziyeke12 <fritziyeke@gmail.com>
2023-02-03 11:53:52 +0000

    I changed quite a bit, need to commit more

---
+++ index.js
@@ -0,0 +1,11 @@
+function func(){
+    navigator.clipboard.writeText("fritziyeke@gmail.com")
+
+    alert("Copied to clipboard")
+}
+
+function func2(){
+    navigator.clipboard.writeText("07724400327")
+
+    alert("Copied to clipboard")
+}
